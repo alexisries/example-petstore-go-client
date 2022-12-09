abbabd2510793dbf4e350861ecd11e13a255c8de
Alexis Ries <ries.alexis@protonmail.com>
2022-12-09 21:35:55 +0100

    add comments

--- main.go
+++ main.go
@@ -7,19 +7,29 @@ import (
 )
 
 func main() {
+	// initialize petstore api client
 	client, err := petstore.NewClientWithResponses("http://localhost:8080/api/v3")
 	if err != nil {
 		panic(err)
 	}
+	// initialize context
 	ctx := context.TODO()
-	pet, err := client.GetPetByIdWithResponse(ctx, 100)
+
+	// get first Pet (id: 1) object
+	petResponse, err := client.GetPetByIdWithResponse(ctx, 1)
 	if err != nil {
 		panic(err)
 	}
-	statuscode := pet.StatusCode()
-	if statuscode != 200 {
-		panic(fmt.Sprintf("error %d from server : %s", statuscode, string(pet.Body)))
-	}
-	fmt.Println(pet.JSON200.Name)
 
+	// check errors
+	statuscode := petResponse.StatusCode()
+	if statuscode != 200 {
+		panic(fmt.Sprintf("error %d from server : %s", statuscode, string(petResponse.Body)))
+	}
+
+	// Unmarshall response json to Pet object
+	var pet *petstore.Pet = petResponse.JSON200
+
+	// Print 'name' attribute of Pet
+	fmt.Println(pet.Name)
 }
